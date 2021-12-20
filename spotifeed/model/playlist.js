c66972176bd2fc10bfe262fb954c3d5b7287871f
anthonyguariglia <aguariglia197@gmail.com>
2021-12-20 12:26:08 -0500

    Add CRUD operations for playlists
    Operations include:
    - Create playlist -> POST /user/playlists
    - Delete playlist -> DELETE /user/playlists/:name
    - Add song -> POST /user/playlists/:name/albums/:id
    - Remove song -> PATCH /user/playlists/:name/albums/:id
    - Show songs -> GET /user/playlists/:name
    
    All of these operations require a valid secret_token that is provided on successful sign-in. In other words, you can't view or edit your playlists without signing in first

--- spotifeed/model/playlist.js
+++ spotifeed/model/playlist.js
@@ -7,7 +7,8 @@ const playlistSchema = new mongoose.Schema({
   },
   albums: [
     {
-      type: mongoose.Schema.Types.ObjectId
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Playlist'
     }
   ]
 })
--- spotifeed/routes/secure-routes.js
+++ spotifeed/routes/secure-routes.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const store = require('../../store')
+const Playlist = require('../model/playlist')
+const Artist = require('../model/artist')
+const Album = require('../model/album')
 
+
+const URL = require('../../config.js')
 router.get('/profile', (req, res, next) => {
 	res.json({
 		message: 'You made it to the secure route',
@@ -22,6 +27,95 @@ router.delete('/logout', (req, res, next) => {
 		user: req.user,
 		token: req.query.secret_token,
 	})
+})
+
+// Create playlist
+router.post('/playlists', (req, res, next) =>  {
+	try {
+		Playlist.create({
+			name: req.body.name,
+			artists: store.artists,
+		})
+		.then(playlist => {
+			res.status(201).json(playlist)
+		})
+		.catch(next())
+	} catch(error) {
+		next(error)
+	}
+})
+
+// Delete playlist
+router.delete('/playlists/:name', (req, res, next) => {
+	try {
+		Playlist.find({ name: req.params.name })
+			.then((playlist) => {
+				playlist.deleteOne()
+			})
+			.then(res.status(204))
+			.catch(next())
+	} catch(error) {
+		next(error)
+	}
+})
+
+// add finding name, fix inner data
+// Add album to playlist
+router.post('/playlists/:name/albums/:id', (req, res, next) => {
+	try {
+		const albums = store.albums
+		Album.find({ id: req.params.id })
+			.then((album) => {
+				const albumId = album._id
+			})
+			.then(
+				Playlist.find({ name: req.body.name }).then((playlist) => {
+					playlist[albums].push(albumId)
+					return playlist.save()
+				})
+			)
+			.then(res.status(201).json())
+			.catch(next())
+	} catch (error) {
+		next(error)
+	}
+})
+
+// remove album from playlist
+router.patch('/playlists/:name/albums/:id', (req, res, next) => {
+	try {
+		const albums = store.albums
+		Album.find({ id: req.params.id })
+			.then((album) => {
+				const albumId = album._id
+				const deletedIndex = albums.indexOf(albumId)
+			})
+			.then(
+				Playlist.find({ name: req.body.name })
+					.then((playlist) => {
+						playlist[albums].splice(deletedIndex, 1)
+						return playlist.save()
+					})
+			)
+			.then(res.status(201).json())
+			.catch(next())
+	} catch (error) {
+		next(error)
+	}
+})
+
+// Display playlist data
+router.get('/playlists/:name', (req, res, next) => {
+	try {
+		Playlist.find({ name: req.params.name })
+			.populate('albums')
+			.then((playlist) => {
+				res.status(200).json(playlist)
+			})
+			.catch(next())
+	} catch(error) {
+		next(error)
+	}
 })
 
 module.exports = router
