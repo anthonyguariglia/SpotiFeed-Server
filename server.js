7473e569b32835d76c6fc93b8c907dd86ada9227
anthonyguariglia <aguariglia197@gmail.com>
2021-12-23 11:17:45 -0500

    change file structure to use server.js, not app.js

--- server.js
+++ server.js
@@ -1,19 +1,57 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const jwtRoutes = require('./spotifeed/routes/jwt-routes')
+const spotifyApiRoutes = require('./spotifeed/routes/spotify-api-routes')
+const spotifyAuthRoutes = require('./spotifeed/routes/spotify-auth-routes')
+const secureRoute = require('./spotifeed/routes/secure-routes')
+const cors = require('cors')
+require('./spotifeed/jwt-auth/jwt-auth')
+
+mongoose.connect('mongodb+srv://anthonyguariglia:tony@cluster0.3vskp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+)
+mongoose.set('useCreateIndex', true)
+mongoose.connection.on('error', (error) => console.log(error))
+mongoose.Promise = global.Promise
+
 const app = express()
 
 
-app.get('/login', function (req, res) {
-	var state = generateRandomString(16)
-	var scope = 'user-read-private user-read-email'
 
-	res.redirect(
-		'https://accounts.spotify.com/authorize?' +
-			querystring.stringify({
-				response_type: 'code',
-				client_id: client_id,
-				scope: scope,
-				redirect_uri: redirect_uri,
-				state: state,
-			})
-	)
+
+// const clientDevPort = 7165
+
+// app.use(
+// 	cors({
+// 		origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}`,
+// 	})
+// )
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*')
+	next()
+})
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(jwtRoutes)
+app.use(spotifyApiRoutes)
+app.use(spotifyAuthRoutes)
+// app.use(cors(corsOptions))
+
+// Plug in the JWT strategy as a middleware so only verified users can access this route.
+
+
+app.use('/user', passport.authenticate('jwt', { session: false }), secureRoute)
+
+// Handle errors.
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500)
+	res.json({ error: err })
+})
+
+app.listen(3000, () => {
+	console.log('Server started.')
 })
